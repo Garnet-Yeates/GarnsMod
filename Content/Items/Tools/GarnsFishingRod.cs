b40bc7caa9055f41cc4ea19b5700f70d88d8f87c
Garnet Yeates <garnetyeates8@gmail.com>
2022-09-15 03:05:10 -0400

    Moved GarnsFishingRodPlayer to here

--- Content/Items/Tools/GarnsFishingRod.cs
+++ Content/Items/Tools/GarnsFishingRod.cs
@@ -6,14 +6,16 @@ using ReLogic.Content;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent.Creative;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 using static GarnsMod.Tools.ColorGradient;
-
+using static Terraria.GameContent.ItemDropRules.Chains;
 
 namespace GarnsMod.Content.Items.Tools
 {
@@ -208,7 +210,7 @@ namespace GarnsMod.Content.Items.Tools
         // Called on all clients/server every tick that the item is in their hand
         public override void HoldItem(Player player)
         {
-            level = 4;
+            level = 24;
             player.fishingSkill += FishingPowerAdditiveIncrease;
             if (!Main.dedServ)
             {
@@ -489,6 +491,68 @@ namespace GarnsMod.Content.Items.Tools
             public static bool operator ==(TrailTypeMode m1, TrailTypeMode m2) => m1.Value == m2.Value;
             public static bool operator !=(TrailTypeMode m1, TrailTypeMode m2) => m1.Value != m2.Value;
             public override int GetHashCode() => Value;
+        }
+    }
+
+    // This ModPlayer is used to make things happen when the player fishes specifically with GarnsFishingRod
+    public class GarnsFishingRodPlayer : ModPlayer
+    {
+        // This is used for multiplicatively increasing fishing skill. Works similar to how time of day/moonphase/weather affects it
+        // Pretty sure it is only called on the client who is fishing
+        // For our additive increase we do it in GarnFishingRod.HoldItem
+        public override void GetFishingLevel(Item fishingRod, Item bait, ref float fishingLevelMultiplier)
+        {
+            if (fishingRod.ModItem is GarnsFishingRod rod)
+            {
+                fishingLevelMultiplier += rod.FishingPowerMultIncrease * 0.01f;
+            }
+        }
+
+        // Only called on the client fishing
+        public override void ModifyFishingAttempt(ref FishingAttempt attempt)
+        {
+            // This if block runs if the player is fishing with Garn's fishing rod
+            if (attempt.playerFishingConditions.Pole.ModItem is GarnsFishingRod rod)
+            {
+                // The pole can fish in lava at [LavaFishingLevel]
+                if (rod.level >= GarnsFishingRod.LavaFishingLevel)
+                {
+                    attempt.CanFishInLava = true;
+                }
+
+                // At level [CrateChanceLevel], there is 10% additional chance that the catch will be a crate
+                // The "tier" of the crate depends on the rarity, which we don't modify here (see ExampleMod ExampleFishingPlayer.CatchFish comments)
+                if (!attempt.crate && rod.level >= GarnsFishingRod.CrateChanceLevel)
+                {
+                    if (Main.rand.Next(100) < 10)
+                    {
+                        attempt.crate = true;
+                    }
+                }
+            }
+        }
+
+        // Only called on the client who is fishing
+        public override bool? CanConsumeBait(Item bait)
+        {
+            PlayerFishingConditions conditions = Player.GetFishingConditions(); ;
+
+            // This makes it so there is a multiplicative % decrease in bait consumption. We return false or null which means "dont consume" or "let vanilla decide"
+            if (conditions.Pole.ModItem is GarnsFishingRod rod)
+            {
+                return Main.rand.Next(100) < rod.BaitConsumptionReductionPercent ? false : null;
+            }
+
+            return null; // Let the default vanilla logic run
+        }
+
+        // Only called on the client that is fishing
+        public override void ModifyCaughtFish(Item fish)
+        {
+            if (Player.GetFishingConditions().Pole.ModItem is GarnsFishingRod rod)
+            {
+                rod.OnCatchFish();
+            }
         }
     }
 }
