82d2756cb0f196617ddde28884f1b4530438a3c3
Garnet Yeates <garnetyeates8@gmail.com>
2022-09-14 21:20:50 -0400

    For My Education

---
+++ Content/RandomStuff/CustomItemDropResolver.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+
+namespace GarnsMod.Content.RandomStuff
+{
+    // This is vanilla code copied verbatim so that we can just call ResolveRule recursive logic on any rules/chains that we create
+    internal class CustomItemDropResolver
+    {
+        /* This is simply here to show how vanilla does it. Normally TryDropping is called on a DropAttempt info when an NPC dies or a bag is opened.
+            * How this works is that the DropAttemptInfo either has an NPC set or an item set (depending on if an NPC died or a bagwas opened). It will look
+            * thru the rules database to find out what rules to use for the given NPC or item (npc takes prio),
+            * then call ResolveRule() on them. What I really care about is the ResolveRule function as well as its helper functions. Simply creating a rule and 
+            * calling rule.TryDroppingItem will not work for chained rules and it will also not care about the CanDrop method (i.e conditional rules).
+            * By copying this vanilla logic, we can full-functionally just create drop rules and drop stuff based on these awesome rules/conditions/chains, WITHOUT needing 
+            * an actual an npc or item, Read GetDropAttemptInfo for more info on the whole process and what to avoid/look out for
+            * 
+        public void TryDropping(DropAttemptInfo info)
+        {
+            List<IItemDropRule> rulesForNPCID = ((info.npc != null) ? this._database.GetRulesForNPCID(info.npc.netID) : this._database.GetRulesForItemID(info.item));
+            for (int i = 0; i < rulesForNPCID.Count; i++)
+            {
+                this.ResolveRule(rulesForNPCID[i], info);
+            }
+        }*/
+
+        /**<summary>
+            * When CommonCode.DropItem is called via the Rule being successful and dropping the item, it checks if DropAttemptInfo.npc exists. <br/><br/>
+            * 
+            * If it doesn't, it calls CommonCode.DropItem (different overload than the one I mentioned above) and drops the item with player.GetItemSource_OpenItem 
+            *   where opened item is dirt block. In this case the item is dropped on top of the player <br/><br/>
+            *   
+            * If it does exist, it calls CommonCode._DropItemFromNPC which makes the drop source npc.ItemSource_Loot and the item is dropped within the hitbox of
+            *   the NPC. It also calls CommonCode.ModifyDropFromNPC (for changing things like dropped item color if slime is killed) but this won't do anything if NPC is type 0
+            *   so we don't have to worry about that <br/><br/>
+            *   
+            * Choosing to input the 'hitbox' param will tell my code to trick CommonCode into thinking that this dropped from an NPC of type 0. This allows us to choose
+            * where the item drops instead of making it drop on the player <br/><br/>
+            * 
+            * Keep in mind that this means we shouldn't use rule conditions that require the npc to have x amount of health or value, etc or it won't work properly. I doubt that
+            * would ever be needed anyways. Also keep in mind that when using ResolveRule (below) you are not calling it on MP_Client netmode
+            * </summary>
+            */
+        public static DropAttemptInfo GetDropAttemptInfo(Rectangle? hitbox = null)
+        {
+            int itemDummy = ItemID.DirtBlock;
+            NPC npcDummy = null;
+            if (hitbox is Rectangle box)
+            {
+                npcDummy = new()
+                {
+                    Hitbox = box,
+                };
+            }
+            return new() { npc = npcDummy, item = itemDummy, rng = Main.rand, IsExpertMode = Main.expertMode, IsMasterMode = Main.masterMode, IsInSimulation = false, player = Main.LocalPlayer };
+        }
+
+        // Verbatim vanilla code
+        public static ItemDropAttemptResult ResolveRule(IItemDropRule rule, DropAttemptInfo info)
+        {
+            if (!rule.CanDrop(info))
+            {
+                ItemDropAttemptResult itemDropAttemptResult = default(ItemDropAttemptResult);
+                itemDropAttemptResult.State = ItemDropAttemptResultState.DoesntFillConditions;
+                ItemDropAttemptResult itemDropAttemptResult2 = itemDropAttemptResult;
+                ResolveRuleChains(rule, info, itemDropAttemptResult2);
+                return itemDropAttemptResult2;
+            }
+            ItemDropAttemptResult itemDropAttemptResult3 = (rule as INestedItemDropRule)?.TryDroppingItem(info, ResolveRule) ?? rule.TryDroppingItem(info);
+            ResolveRuleChains(rule, info, itemDropAttemptResult3);
+            return itemDropAttemptResult3;
+        }
+
+        // Verbatim vanilla code
+        private static void ResolveRuleChains(IItemDropRule rule, DropAttemptInfo info, ItemDropAttemptResult parentResult)
+        {
+            ResolveRuleChains(ref info, ref parentResult, rule.ChainedRules);
+        }
+
+        // Verbatim vanilla code
+        private static void ResolveRuleChains(ref DropAttemptInfo info, ref ItemDropAttemptResult parentResult, List<IItemDropRuleChainAttempt> ruleChains)
+        {
+            if (ruleChains == null)
+            {
+                return;
+            }
+            for (int i = 0; i < ruleChains.Count; i++)
+            {
+                IItemDropRuleChainAttempt itemDropRuleChainAttempt = ruleChains[i];
+                if (itemDropRuleChainAttempt.CanChainIntoRule(parentResult))
+                {
+                    ResolveRule(itemDropRuleChainAttempt.RuleToChain, info);
+                }
+            }
+        }
+    }
+}
---
+++ Content/RandomStuff/RandomStuff.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Chat;
+using Terraria.GameContent.Achievements;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static Terraria.GameContent.ItemDropRules.Chains;
+
+namespace GarnsMod.Content.RandomStuff
+{
+    internal class TileTest : GlobalTile
+    {
+
+        // Pretty sure this isn't called on MultiplayerClient Netmode so I omitted the check. Could be wrong tho :)
+        public override bool Drop(int tileX, int tileY, int type)
+        {
+            // Return true if it isn't shadow orb/crimson heart as we don't want to modify logic for other tile such as life crystal
+            if (type != TileID.ShadowOrbs)
+            {
+                return true;
+            }
+
+            // When you break the tile you 'initiate' the breaking, but WorldGen is actually destroying the 4 segmentsof this tile.
+            // This whole method gets called 5 times but only one of those times is the time where YOU initiated the breaking.
+            // WorldGen will set destroyObject to true and do the actual deleting of the tile. This basically makes sure this is called once and you
+            // don't get 5x the intended effects
+            if (WorldGen.destroyObject)
+            {
+                return false;
+            }
+
+            // I think shadow orb and crimson heart use the same texture just different animation frames so they use this to check which one it is (shadow or crim)
+            // 16px with 2 px of padding makes 18x18px frames. Pixels from 0 to 17 are for the left side of the shadow orb. 18 to 35 are the right side of it
+            // 36 to 53 are left side of crimson heart... etc. They use this to figure out whether a crimson heart or shadow orb is being smashed
+            short frameX = Main.tile[tileX, tileY].TileFrameX;
+
+            bool isCrimsonHeart = frameX >= 36;
+
+            // This code adjusts the tileX and Y that we broke to make it the top left of the tile
+            int x = ((Main.tile[tileX, tileY].TileFrameX != 0 && Main.tile[tileX, tileY].TileFrameX != 36) ? (tileX - 1) : tileX);
+            int y = ((Main.tile[tileX, tileY].TileFrameY != 0) ? (tileY - 1) : tileY);
+
+            if (isCrimsonHeart) // CRIMSON HEART LOGIC
+            {
+                // TODO either replace one of the choices, add another choice, or both, or add an additional drop on top of the choice. Or all 3. You see where I'm going with this
+                int choice = Main.rand.Next(5);
+                if (!WorldGen.shadowOrbSmashed)
+                {
+                    choice = 0; // Always drop Undertaker if it is the first one smashed
+                }
+                switch (choice)
+                {
+                    case 0:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.TheUndertaker, 1, noBroadcast: false, -1);
+                        int stack = WorldGen.genRand.Next(100, 101);
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.MusketBall, stack);
+                        break;
+                    case 1:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.CrimsonRod, 1, noBroadcast: false, -1);
+                        break;
+                    case 2:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.TheRottedFork, 1, noBroadcast: false, -1);
+                        break;
+                    case 3:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.CrimsonHeart, 1, noBroadcast: false, -1);
+                        break;
+                    case 4:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.PanicNecklace, 1, noBroadcast: false, -1);
+                        break;
+                }
+            }
+            else // SHADOW ORB LOGIC
+            {
+                // TODO either replace one of the choices, add another choice, or both, or add an additional drop on top of the choice. Or all 3. You see where I'm going with this
+
+                // Example of me using this. Make sure it is never called on mp client netmode
+                List<IItemDropRule> rulesToTry = new()
+                {
+                    new CommonDrop(ItemID.DirtBlock, 1, 20, 60),
+                    new CoinsRule(Item.sellPrice(0, 1, 0, 0), true),
+                    new OneFromRulesRule
+                    (
+                        chanceDenominator: 1, chanceNumerator: 1,
+                        new CommonDrop(ItemID.SoulofFright, 1, 5, 10),
+                        new CommonDrop(ItemID.SoulofMight, 1, 5, 10),
+                        new CommonDrop(ItemID.SoulofSight, 1, 5, 10),
+                        new CommonDrop(ItemID.SoulofLight, 1, 5, 10),
+                        new CommonDrop(ItemID.SoulofNight, 1, 5, 10),
+                        new CommonDrop(ItemID.SoulofFlight, 1, 5, 10)
+                    )
+                };
+                rulesToTry.ForEach(rule => CustomItemDropResolver.ResolveRule(rule, CustomItemDropResolver.GetDropAttemptInfo(new Rectangle(x*16, y*16, 32, 32))));
+
+                /*
+                int choice = Main.rand.Next(9);
+
+                if (!WorldGen.shadowOrbSmashed)
+                {
+                    choice = 0; // Always drop musket if it is the first one smashed
+                }
+                switch (choice)
+                {
+                    case 0:
+                        {
+                            Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.Musket, 1, noBroadcast: false, -1);
+                            int stack2 = WorldGen.genRand.Next(100, 101);
+                            Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.MusketBall, stack2);
+                            break;
+                        }
+                    case 1:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.Vilethorn, 1, noBroadcast: false, -1);
+                        break;
+                    case 2:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.BallOHurt, 1, noBroadcast: false, -1);
+                        break;
+                    case 3:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.ShadowOrb, 1, noBroadcast: false, -1);
+                        break;
+                    case 4:
+                        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, y * 16, 32, 32, ItemID.BandofStarpower, 1, noBroadcast: false, -1);
+                        break;
+                }*/
+            }
+
+            // Has to do with spawing the corresponding boss and putting text in the chat. If you forget this part then the bosses wont spawn
+            WorldGen.shadowOrbSmashed = true;
+            WorldGen.shadowOrbCount++;
+            if (WorldGen.shadowOrbCount >= 3)
+            {
+                // They wrote this statement in a confusing way where they distributed a negation on the right side but didnt on the left side, so removed the distribution completely
+                // It is just making sure that the corresponding boss isn't already alive. If it is, nothing happens
+                if (!(NPC.AnyNPCs(NPCID.BrainofCthulhu) && isCrimsonHeart) && !(NPC.AnyNPCs(NPCID.EaterofWorldsHead) && !isCrimsonHeart))
+                {
+                    WorldGen.shadowOrbCount = 0;
+                    float num5 = x * 16;
+                    float num6 = y * 16;
+                    float num7 = -1f;
+                    int plr = 0;
+                    for (int num8 = 0; num8 < 255; num8++)
+                    {
+                        float num9 = Math.Abs(Main.player[num8].position.X - num5) + Math.Abs(Main.player[num8].position.Y - num6);
+                        if (num9 < num7 || num7 == -1f)
+                        {
+                            plr = num8;
+                            num7 = num9;
+                        }
+                    }
+                    if (isCrimsonHeart)
+                    {
+                        NPC.SpawnOnPlayer(plr, 266);
+                    }
+                    else
+                    {
+                        NPC.SpawnOnPlayer(plr, 13);
+                    }
+                }
+            }
+            else
+            {
+                LocalizedText localizedText = Lang.misc[10];
+                if (WorldGen.shadowOrbCount == 2)
+                {
+                    localizedText = Lang.misc[11];
+                }
+                if (Main.netMode == NetmodeID.SinglePlayer)
+                {
+                    Main.NewText(localizedText.ToString(), 50, byte.MaxValue, 130);
+                }
+                else if (Main.netMode == NetmodeID.Server)
+                {
+                    ChatHelper.BroadcastChatMessage(NetworkText.FromKey(localizedText.Key), new Color(50, 255, 130));
+                }
+            }
+            AchievementsHelper.NotifyProgressionEvent(7); return false; // Return false so that the vanilla logic doesn't run
+        }
+    }
+
+    class MyNPCLoot : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (npc.type == NPCID.IceMimic)
+            {
+                foreach (var rule in npcLoot.Get())
+                {
+                    if (rule is CommonDrop commonDrop)
+                    {
+                        foreach (var chainedRule in commonDrop.ChainedRules)
+                        {
+                            if (chainedRule is TryIfFailedRandomRoll tryIfFailedRoll && tryIfFailedRoll.RuleToChain is OneFromOptionsDropRule ofoDrop && ofoDrop.dropIds.Contains(ItemID.Frostbrand))
+                            {
+                                List<int> dropIds = ofoDrop.dropIds.ToList();
+                                dropIds.Remove(ItemID.Frostbrand);
+                                ofoDrop.dropIds = dropIds.ToArray();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    
+}
