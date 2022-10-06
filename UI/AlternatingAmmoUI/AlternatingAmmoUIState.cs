604f195fe63eb9f3d8ac3ae9cc384321f260eb09
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2022-10-06 05:18:23 -0400

    Bug Fix

--- UI/AlternatingAmmoUI/AlternatingAmmoUIState.cs
+++ UI/AlternatingAmmoUI/AlternatingAmmoUIState.cs
@@ -29,6 +29,8 @@ namespace GarnsMod.UI.AlternatingAmmoUI
 
         public override void Update(GameTime gameTime)
         {
+            base.Update(gameTime);
+
             CalculateOrigin();
         }
 
