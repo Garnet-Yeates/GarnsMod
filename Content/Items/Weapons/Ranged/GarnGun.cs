e948c167a10b366bb1e6770198960ae28f781dc0
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2022-10-06 05:20:13 -0400

    Added Special Gun
    - Can shoot stynger bolts with right click
    - Shoots regular bullet ammo with left click
    - Ammo counts show combined ammo
    - When one of your ammunitions runs out (either stynger or bullet) ammo count will temporarily show a 0

--- Content/Items/Weapons/Ranged/GarnGun.cs
+++ Content/Items/Weapons/Ranged/GarnGun.cs
@@ -129,7 +129,6 @@ namespace GarnsMod.Content.Items.Weapons.Ranged
 
     public class GarnGunSpawnedProjectile : GlobalProjectile
     {
-        // Unlike ModProjectile.OnSpawn, GlobalProjectile.OnSpawn is called on all clients (not just owner)
         public override void OnSpawn(Projectile projectile, IEntitySource source)
         {
             if (source is EntitySource_ItemUse_WithAmmo { Context: "GarnGunArrow" })
@@ -159,14 +158,12 @@ namespace GarnsMod.Content.Items.Weapons.Ranged
         }
     }
 
-
-
     // This class makes it so that:
     // - Left clicking shoots bullet
     // - Right clicking shoots stynger bolt
     //
-    // It does some slightly advanced engine logic to make it so that when ItemSlots are drawn, the displayed ammunition count shows [stynger bolt counts + bullet counts] instead of [bullet counts],
-    // with a weird [stynger bolt count] for one frame after a stynger bolt is shot
+    // It does some slightly advanced engine hacky-logic to control exactly which calls to ItemLoader.CanChooseAmmo are affected. This make it so that when ItemSlots are drawn,
+    // the displayed ammunition count shows [stynger bolt counts + bullet counts] instead of [bullet counts], with a weird [stynger bolt count] for one frame after a stynger bolt is shot
     public class GarnGunHackyLogic : ModItem
     {
         public override string Texture => $"{nameof(GarnsMod)}/Content/Items/Weapons/Ranged/GarnGun";
@@ -205,7 +202,7 @@ namespace GarnsMod.Content.Items.Weapons.Ranged
             Item.useAmmo = AmmoID.Bullet;
         }
 
-        // Allow right-click use
+        // Allow alt function use
         public override bool AltFunctionUse(Player player)
         {
             return true;
@@ -213,32 +210,48 @@ namespace GarnsMod.Content.Items.Weapons.Ranged
 
         // This class does a lot of "engine dancing" if you will. CanChooseAmmo hooks are called in many different parts of the engine (One to display ammo counts, one to decide if item can be used (ItemCheck_CheckCanUse),
         // and one to pick the ammo that is actually shot (PickAmmo, inside ItemCheck_Shoot) as well as in many other places (seeing it a lot in Projectile.cs). We use withinShootLogic flag to figure out where we are
-        // in the engine when these hooks are called. This allows us to make ammo counts display combinations of ammos, while making shoot logic restricted to using one type of ammo based on alt function. 
+        // in the engine when these hooks are called. This allows us to make CanChooseAmmo behave differently based on where it is called (i.e for ammo counts: display combinations of ammos, for shooting/using item logic:
+        // restrict: to using one type of ammo / item based on alt function. Set to true at the beginning of ItemUse_CheckItemUse and will be set to false when Shoot() hook is called (or timeout if shoot hook is never called)
+        // 'withinShootLogic' means that we are somewhere in ItemCheck_CheckCanUse or somewhere in ItemCheck_Shoot (ItemCheck_Shoot is procedurally called a bit after ItemCheck_CheckCanUse)
         private bool withinShootLogic;
 
-        // CanShoot is the first hook called within ItemCheck_Shoot
-        public override bool CanShoot(Player player)
-        {
-            withinShootLogic = true;
-            return true;
-        }
+        // withinShootLogic should naturally be set to false when Shoot() is called (end of shoot logic). However, after the initial CanUseItem where it is set to true, there are things
+        // that can happen along the way that cause Shoot() to not be called. Mainly it could be a different CanUseItem() hook returning false at the beginning of
+        // ItemCheck_CheckCanUse, or it could be the CanShoot() hook returning false at the beginning of ItemCheck_Shoot. It could also be from the item not having the ammo required to shoot which would also cause
+        // ItemCheck_Shoot to fail. Either way we do not want to be considered to be 'inside' shoot logic for a long period, so this is to ensure of that. (if we stay 'inside' shoot logic, the item slot will display
+        // the counts for the main ammo if altFunction0 was used last, or they will display the count for the alternate ammo if altFunction2 was used last. We want it to display the 'combined' ammunitions.
+        // This also comes with an amazing side effect of showing [0] when you run out of either main ammo or alt ammo and are trying to shoot said ammo, instead of showing [mainAmmo + altAmmo]
+        private int shootLogicTimeout = 0;
 
-        // So when ItemCheck_CheckCanUse happens, it is outside of shoot logic (happens before ItemCheck_Shoot), meaning if the player has stynger bolt OR bullet in their inventory, it will allow the item to be used
-        // HOWEVER it is not guaranteed that it will shoot anything when it's used (what if they have stynger bolts in their inv, but 0 bullets, and they left click to shoot a bullet? What will
-        //    happen is that ItemCheck_CheckCanUse will allow it to be used, but once we are in the shoot logic, we narrow down what can actually be shot depending on alt function so it might shoot nothing.
-        //    this creates a visual issue of the gun being held out trying to be used, but ItemCheck_Shoot determines it can't be shot
-        // Having this check here actually makes sure the item won't be used if it is going to shoot nothing. We are actually using ItemLoader which is what tModLoader uses to
+        // For all intents and purposes, this is exactly equivalent to 'player.altFunctionUse == 2'. We set it at the beginning of item use, and it times out shortly after item use (similar to how withinShootLogic times out)
+        // We want to keep 'usingAltFunction' true for a little bit after shoot logic ends if it was initially true (which is why it times out with withinShootLogic, as opposed to letting vanilla set it back to 0 immediately after item use).
+        // This is so if shoot logic ends pre-emptively (some time after CanUseItem hook, but before Shoot hook), during the time that withinShootLogic has not timed out yet, it temporarily shows us the ammo count of
+        // [alt ammo] as opposed to showing [main ammo] <= (Normally player.altFunctionUse would always be 0 inside my CanChooseAmmo during the time period where withinShootLogic is still true due to shoot logic ending early,
+        // but we save it for a bit)
+        private bool usingAltFunction;
+
+        // We want to be 'withinShootLogic' for  when:
+        // ItemCheck_CheckCanUse is called in tModloader source
+        // ItemCheck_Shoot is called in tModloader source. 
         public override bool CanUseItem(Player player)
         {
             withinShootLogic = true;
-            bool found = false;
-            for (int i = 0; i < 58; i++)
-                if (player.inventory[i].type != ItemID.None && player.inventory[i].ammo != 0)
-                    found |= ItemLoader.CanChooseAmmo(Item, player.inventory[i], player);
-            withinShootLogic = false;
-            return found;
+            usingAltFunction = player.altFunctionUse == 2;
+            shootLogicTimeout = 25;
+            return true;
         }
 
+        public override void UpdateInventory(Player player)
+        {
+            // These two flags are set at the very beginning of item use, and are naturally set to false at the very end of the shoot logic (when Shoot hook is called). 
+            // However it is possible that shoot logic ended early without Shoot hook being called. This is why we have them time out here. If they never timed out, it would
+            // mess with the displayed ammunition counts because it *thinks* we're in "shoot logic" but we're really in "display ammo counts logic"
+            if (--shootLogicTimeout == 0)
+            {
+                usingAltFunction = false;
+                withinShootLogic = false;
+            }
+        }
 
         public override bool? CanChooseAmmo(Item ammoItem, Player player)
         {
@@ -258,7 +271,7 @@ namespace GarnsMod.Content.Items.Weapons.Ranged
             // Down here we are within shoot logic
 
             // if altFunctionUse is 0, we only allow bullets
-            if (player.altFunctionUse == 0)
+            if (!usingAltFunction)
             {
                 return ammoItem.ammo == AmmoID.Bullet;
             }
@@ -267,9 +280,13 @@ namespace GarnsMod.Content.Items.Weapons.Ranged
             return ammoItem.type == ItemID.StyngerBolt;
         }
 
+        // We reset our withinShootLogic and usingAltFunction here normally (this is where I consider the "shoot logic" to be over). It is not guaranteed that
+        // Shoot() will be called every time ItemCheck_CheckCanUse is called (i.e if they have no ammo left based on the alt function), so in that case it times
+        // out after 25 ticks
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
             withinShootLogic = false;
+            usingAltFunction = false;
             return true;
         }
     }
